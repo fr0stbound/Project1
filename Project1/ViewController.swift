9e5c4ea0eba3a4d27af6e2b24d4b67ee468a0642
fr0stbound <frostbound9@gmail.com>
2023-04-10 18:00:39 +0500

    no message

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
     
     override func viewDidLoad() {
@@ -21,9 +21,27 @@ class ViewController: UIViewController {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
             }
-            print(pictures)
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
         }
     }
-
+    
 }
 
