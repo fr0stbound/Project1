7b676c8ff076a1dfb2763ed8ceb269376e56db00
fr0stbound <frostbound9@gmail.com>
2023-04-11 20:24:18 +0500

    no message

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         // Do any additional setup after loading the view.
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
